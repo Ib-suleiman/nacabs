20cea4ab468ea941ea1f0eb35eef85314890c5eb
Ibrahim Suleiman <ibsuleiman123@gmail.com>
2025-09-23 14:06:48 +0100

    Collect static files

--- nacabs/settings.py
+++ nacabs/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]  # optional for global static
+
+# Media files (for user uploads like images)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
