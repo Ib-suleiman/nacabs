e1b6cb05b0700d10fafa997650618d644ef31506
Ibrahim Suleiman <ibsuleiman123@gmail.com>
2025-09-27 06:48:31 +0100

    Prepare for Render deployment

--- nacabs/settings.py
+++ nacabs/settings.py
@@ -136,10 +136,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ibrahimsuleiman77@gmail.com'
-# EMAIL_HOST_PASSWORD = 'owtqbrhfnajfdtzw'
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') 
+EMAIL_HOST_PASSWORD = 'owtqbrhfnajfdtzw'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com', 'nacabspoly.onrender.com']
